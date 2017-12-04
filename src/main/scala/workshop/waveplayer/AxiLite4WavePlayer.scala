dbef5a068bbe8c3234b549210abfec4f58db18eb
Dolu1990 <papuchoud@hotmail.com>
2017-12-04 14:51:39 +0100

    Add AxiLite4WavePlayerMultiTone

--- src/main/scala/workshop/waveplayer/AxiLite4WavePlayer.scala
+++ src/main/scala/workshop/waveplayer/AxiLite4WavePlayer.scala
@@ -6,7 +6,7 @@ import spinal.lib.bus.amba4.axilite.{AxiLite4, AxiLite4SlaveFactory}
 class AxiLite4WavePlayer(wavePlayerGenerics : WavePlayerGenerics) extends Component{
   import wavePlayerGenerics._
   val io = new Bundle{
-    val axiLite = slave(AxiLite4(addressWidth = 2*sampleCountLog2, dataWidth = 32))
+    val axiLite = slave(AxiLite4(addressWidth = 2*sampleCountLog2, dataWidth = 32)) //2*sampleCountLog2 would allow, eventualy, to map the ROM into the memory space (optional)
     val wave = out(Sample)
   }
 
---
+++ src/main/scala/workshop/waveplayer/AxiLite4WavePlayerMultiTone.scala
@@ -0,0 +1,44 @@
+package workshop.waveplayer
+
+import spinal.core._
+import spinal.lib._
+import spinal.lib.bus.amba4.axilite.{AxiLite4, AxiLite4SlaveFactory}
+import spinal.lib.bus.misc.BusSlaveFactoryAddressWrapper
+
+class AxiLite4WavePlayerMultiTone(wavePlayerGenerics : WavePlayerGenerics, toneCount : Int) extends Component{
+  import wavePlayerGenerics._
+  val io = new Bundle{
+    val axiLite = slave(AxiLite4(addressWidth = 2*sampleCountLog2*log2Up(toneCount), dataWidth = 32))
+    val wave = out(Sample)
+  }
+
+  val factory = new AxiLite4SlaveFactory(io.axiLite)
+
+  //Instanciate one wavePlayer and one WavePlayerMapper for each tones
+  val tones = for(i <- 0 until toneCount) yield new Area {
+    val wavePlayer = new WavePlayer(wavePlayerGenerics)
+    //BusSlaveFactoryAddressWrapper allow to create a fake factory which will apply an address offset to each primitives calls
+    val factoryWithOffset = new BusSlaveFactoryAddressWrapper(factory,i*2*sampleCountLog2)
+    val mapper = new WavePlayerMapper(factoryWithOffset, wavePlayer)
+  }
+
+  //Take all tones and add their output together (add zero for the ones which aren't running)
+  io.wave := tones.map(tone => tone.wavePlayer.phase.run ? tone.wavePlayer.filter.value | U(0)).reduce(_ + _)
+}
+
+
+object AxiLite4WavePlayerMultiTone{
+  def main(args: Array[String]): Unit = {
+    SpinalConfig(targetDirectory = "rtl").generateVerilog(
+      gen = new AxiLite4WavePlayerMultiTone(
+        wavePlayerGenerics = WavePlayerGenerics(
+          sampleWidth = 16,
+          sampleCountLog2 = 5,
+          phaseWidth = 16,
+          filterCoefWidth = 8
+        ),
+        toneCount = 4
+      )
+    )
+  }
+}
