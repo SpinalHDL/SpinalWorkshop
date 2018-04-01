55476af9f6757442ffe801213baa6180dbdd823d
Dolu1990 <papuchoud@hotmail.com>
2018-04-01 19:43:21 +0200

    Fix compilation

--- src/main/scala/workshop/counter/Counter.scala
+++ src/main/scala/workshop/counter/Counter.scala
@@ -3,8 +3,11 @@ package workshop.counter
 import spinal.core._
 
 case class Counter(width: Int) extends Component {
-  val io = new Bundle{
-    //TODO define inputs/outputs
+  val io = new Bundle {
+    val clear    = in  Bool
+    val value    = out UInt(width bits)
+    val full     = out Bool
   }
+
   //TODO define the logic
 }
