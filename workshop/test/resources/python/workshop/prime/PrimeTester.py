caadd57d33f804e30068639b70bd0df933180e2b
Matthias Wächter <matthias@waechter.wiz.at>
2020-06-22 15:50:48 +0200

    fix PrimeTester for 2to3 transition

--- workshop/test/resources/python/workshop/prime/PrimeTester.py
+++ workshop/test/resources/python/workshop/prime/PrimeTester.py
@@ -6,7 +6,7 @@ from cocotb.triggers import RisingEdge, Timer
 from cocotblib.misc import ClockDomainAsyncReset, simulationSpeedPrinter, assertEquals, SimulationTimeout, randBits
 
 def is_prime(a):
-    return all(a % i for i in xrange(2, a))
+    return all(a % i for i in range(2, a))
 
 @cocotb.test()
 def test1(dut):
@@ -18,5 +18,3 @@ def test1(dut):
        dut.io_value <= randValue
        yield Timer(1000)
        assertEquals(dut.io_isPrime, is_prime(randValue),"fail with " + str(randValue) + " which should have produce a isPrime=" + str(is_prime(randValue)))
-
-
